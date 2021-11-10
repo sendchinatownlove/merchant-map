d1659ec6fc1bca2413d34a18866a94822df63537
billy-yuan <billy.yuan1@gmail.com>
2021-11-10 10:56:23 -0500

    add merchants mock data to app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,47 @@
-import Map from "./map/Map";
+import { LatLong, Map } from "./map/Map";
 import SideBar from "./side-bar/SideBar";
 import "./App.css";
 
 export type Merchant = {
-	name: string;
-	addressLine1: string;
-	addressLine2: string;
-	phoneNumber: string;
-	websiteUrl: string;
-	description: string;
-}
+  name: string;
+  addressLine1: string;
+  addressLine2: string;
+  phoneNumber: string;
+  websiteUrl: string;
+  description: string;
+  position: LatLong;
+};
 
 // Replace when we setup the API call
 const mockMerchants: Merchant[] = [
   {
-    name: 'Chef Katsu',
-    addressLine1: '345 Fulton Street',
-    addressLine2: 'Brooklyn, NY 11222',
-    phoneNumber: '696-420-4200',
-    websiteUrl: 'https://www.reddit.com/r/nba',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quam imperdiet ultricies massa in eget sit tellus. Odio neque diam commodo, eget magna id odio vitae purus. Erat semper in ligula amet gravida tellus.'
+    name: "Chef Katsu",
+    addressLine1: "143 Greene Ave",
+    addressLine2: "Brooklyn, NY 11238",
+    phoneNumber: "696-420-4200",
+    websiteUrl: "https://www.reddit.com/r/nba",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quam imperdiet ultricies massa in eget sit tellus. Odio neque diam commodo, eget magna id odio vitae purus. Erat semper in ligula amet gravida tellus.",
+    position: { lat: 40.6868, lng: -73.9685065 },
   },
+
   {
-    name: 'Win Son',
-    addressLine1: '235 Mortimer Street',
-    addressLine2: 'Brooklyn, NY 101023',
-    phoneNumber: '941-321-3123',
-    websiteUrl: 'https://www.reddit.com/r/movingtojapan',
-    description: 'Bacon ipsum dolor amet alcatra pork shankle, picanha jerky filet mignon brisket beef ribs strip steak. Turkey shoulder ham, shankle chuck tenderloin bacon frankfurter alcatra tail cupim boudin. Swine bacon chuck prosciutto. Short loin bresaola burgdoggen salami andouille capicola short ribs hamburger frankfurter pork rump kevin biltong landjaeger filet mignon.'
-  }
-]
+    name: "Win Son",
+    addressLine1: "159 Graham Avenue",
+    addressLine2: "Brooklyn, NY 11206",
+    phoneNumber: "941-321-3123",
+    websiteUrl: "https://www.reddit.com/r/movingtojapan",
+    description:
+      "Bacon ipsum dolor amet alcatra pork shankle, picanha jerky filet mignon brisket beef ribs strip steak. Turkey shoulder ham, shankle chuck tenderloin bacon frankfurter alcatra tail cupim boudin. Swine bacon chuck prosciutto. Short loin bresaola burgdoggen salami andouille capicola short ribs hamburger frankfurter pork rump kevin biltong landjaeger filet mignon.",
+    position: { lat: 40.7074661, lng: -73.9456839 },
+  },
+];
 
 function App() {
   return (
     <>
       <SideBar merchants={mockMerchants} />
-      <Map />
+      <Map merchants={mockMerchants} />
     </>
   );
 }
--- src/map/Map.tsx
+++ src/map/Map.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { Marker, GoogleMap, LoadScript } from "@react-google-maps/api";
+import { Merchant } from "../App";
 
 // Create an .env file and store your Google Maps API key as VITE_GOOGLE_MAPS_API_KEY
 const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
@@ -8,21 +9,24 @@ const mapContainerStyle = {
   height: "400px",
 };
 
-// Sample payload. Lat-long coordinates are required for the Google maps API
-const merchants = [
-  { name: "Store 1", position: { lat: 40.665708, lng: -73.982378 } },
-  {
-    name: "Store 2",
-    position: { lat: 40.6586321, lng: -73.9843882 },
-  },
-  { name: "Store 3", position: { lat: 40.7109997, lng: -73.9670851 } },
-  { name: "Store 4", position: { lat: 40.7278856, lng: -73.9592555 } },
-  { name: "Store 5", position: { lat: 40.624164, lng: -74.0331407 } },
-  { name: "Store 6", position: { lat: 40.6608971, lng: -73.955495 } },
-];
+export type LatLong = {
+  lat: number;
+  lng: number;
+};
 
-function Map() {
-  const [mapCenter, setMapCenter] = useState(merchants[0].position);
+const defaultMapCenter: LatLong = {
+  lat: 40.7160816,
+  lng: -74.0012583,
+};
+
+interface MapProps {
+  merchants: Merchant[];
+}
+
+export function Map({ merchants }: MapProps) {
+  const [mapCenter, setMapCenter] = useState(
+    merchants.length > 0 ? merchants[0].position : defaultMapCenter
+  );
   const [mapObject, setMapObject] = useState<google.maps.Map | null>(null);
 
   // Force render so that the map pans
@@ -40,7 +44,7 @@ function Map() {
     <LoadScript googleMapsApiKey={apiKey}>
       <GoogleMap
         mapContainerStyle={mapContainerStyle}
-        center={merchants[0].position}
+        center={mapCenter}
         zoom={12}
         onLoad={(map) => {
           setMapObject(map);
@@ -57,5 +61,3 @@ function Map() {
     </LoadScript>
   );
 }
-
-export default Map;
