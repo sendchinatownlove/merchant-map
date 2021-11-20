07cb9b48ef06d3168a8295382db8886304b4de70
billy-yuan <billy.yuan1@gmail.com>
2021-11-20 16:42:44 -0500

    change desktop and mobile cards into jsx elements

--- src/side-bar/SideBar.tsx
+++ src/side-bar/SideBar.tsx
@@ -4,27 +4,51 @@ import { Merchant } from "../utilities/types";
 import { useCheckIfMobile } from "../utilities/useCheckIfMobile";
 import { MerchantCarousel } from "./mobile/MerchantCarousel";
 import { useEventHandler } from "../utilities/EventHandlerContext";
+import React from "react";
 
 interface SideBarProps {
   merchants: Merchant[];
 }
 
-function DesktopCards(merchants: Merchant[]) {
-  return merchants.map((merchant) => <MerchantCard merchant={merchant} />);
+interface DesktopCardsProps {
+  merchants: Merchant[];
 }
 
-function MobileCards(merchants: Merchant[], markedMerchant: Merchant | null) {
+interface MobileCardsProps {
+  merchants: Merchant[];
+  markedMerchant: Merchant | null;
+}
+function DesktopCards({ merchants }: DesktopCardsProps) {
+  return (
+    <>
+      {merchants.map((merchant) => (
+        <MerchantCard merchant={merchant} />
+      ))}
+    </>
+  );
+}
+
+function MobileCards({ merchants, markedMerchant }: MobileCardsProps) {
   if (merchants.length === 0) {
     return <div />;
   }
   if (!markedMerchant) {
     return <MerchantCarousel index={0} merchants={merchants} />;
   }
-  return merchants.map((merchant, index) => {
-    if (merchant.name === markedMerchant?.name) {
-      return <MerchantCarousel index={index} merchants={merchants} />;
-    }
-  });
+  return (
+    <>
+      {merchants.map((merchant, index) => {
+        if (merchant.name === markedMerchant?.name)
+          return (
+            <MerchantCarousel
+              key={"Carousel_" + merchant.name + index}
+              index={index}
+              merchants={merchants}
+            />
+          );
+      })}
+    </>
+  );
 }
 
 function SideBar({ merchants }: SideBarProps) {
@@ -34,9 +58,14 @@ function SideBar({ merchants }: SideBarProps) {
   return (
     <>
       <h1>Explore our merchants</h1>
-      {isMobile
-        ? MobileCards(merchants, state.markedMerchant)
-        : DesktopCards(merchants)}
+      {isMobile ? (
+        <MobileCards
+          merchants={merchants}
+          markedMerchant={state.markedMerchant}
+        />
+      ) : (
+        <DesktopCards merchants={merchants} />
+      )}
     </>
   );
 }
