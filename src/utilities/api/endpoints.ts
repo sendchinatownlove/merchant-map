fde5c2623ddec2b38ccc19158978b2b93bd69d5d
billy-yuan <billy.yuan1@gmail.com>
2021-11-26 13:53:49 -0500

    added strong type to api call

--- src/utilities/api/endpoints.ts
+++ src/utilities/api/endpoints.ts
@@ -1,4 +1,2 @@
-// export const MERCHANTS_ENDPOINT =
-//   "https://us-central1-sendchinatownlove.cloudfunctions.net/merchants";
-
-export const MERCHANTS_ENDPOINT = "http://localhost:8080/";
+export const MERCHANTS_ENDPOINT =
+  "https://us-central1-sendchinatownlove.cloudfunctions.net/merchants";
