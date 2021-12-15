2c601045bb31700cd911111550fee42e9523e870
billy-yuan <billy.yuan1@gmail.com>
2021-12-15 01:13:21 -0500

    set a min-width for the sidebar + update results text to match figma

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,31 +3,12 @@ import { Merchant } from "../../utilities/types";
 import "./Header.scss";
 
 interface HeaderProps {
-  merchants: Merchant[]; 
+  merchants: Merchant[];
 }
 
 interface MerchantCountProps {
   currentCount: number | undefined;
   numberOfMerchants: number | undefined;
-}
-
-function MerchantCount({
-  currentCount,
-  numberOfMerchants
-}: MerchantCountProps) {
-
-  if (
-    currentCount !== undefined &&
-    numberOfMerchants !== undefined
-  ) {
-    return (
-        <div className="Header--Filter-Count">
-          {currentCount} out of {numberOfMerchants}
-        </div>
-    );
-  }
-
-  return <div />;
 }
 
 export function Header({ merchants }: HeaderProps) {
@@ -37,15 +18,17 @@ export function Header({ merchants }: HeaderProps) {
     <div className="Header">
       <h1 className="Header--Title">Explore our merchants</h1>
       <div className="Header--Filters">
-        
         <div className="Header--Category-Container">
-          <div className="Header--Category-Dropdown">Location</div>
-          <div className="Header--Category-Dropdown">Category</div>
-          <div className="Header--Category-Dropdown">Cuisine</div>
+          <div className="Header--Buttons-Container">
+            <div className="Header--Category-Dropdown">Location</div>
+            <div className="Header--Category-Dropdown">Category</div>
+            <div className="Header--Category-Dropdown">Cuisine</div>
+          </div>
+          <div className="Header--Filter-Count">
+            {`${merchants.length} results`}
+          </div>
         </div>
-        <MerchantCount currentCount={state.currentIndex} numberOfMerchants={merchants.length}/>
       </div>
-
     </div>
   );
 }
