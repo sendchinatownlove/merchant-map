9dc3d495d75d12a941ab43d21bc37ef719bdb2bd
billy-yuan <billy.yuan1@gmail.com>
2021-12-15 01:34:51 -0500

    reduce and simplify header css

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -18,15 +18,13 @@ export function Header({ merchants }: HeaderProps) {
     <div className="Header">
       <h1 className="Header--Title">Explore our merchants</h1>
       <div className="Header--Filters">
-        <div className="Header--Category-Container">
-          <div className="Header--Buttons-Container">
-            <div className="Header--Category-Dropdown">Location</div>
-            <div className="Header--Category-Dropdown">Category</div>
-            <div className="Header--Category-Dropdown">Cuisine</div>
-          </div>
-          <div className="Header--Filter-Count">
-            {`${merchants.length} results`}
-          </div>
+        <div className="Header--Buttons-Container">
+          <div className="Header--Category-Dropdown">Location</div>
+          <div className="Header--Category-Dropdown">Category</div>
+          <div className="Header--Category-Dropdown">Cuisine</div>
+        </div>
+        <div className="Header--Filter-Count">
+          {`${merchants.length} results`}
         </div>
       </div>
     </div>
